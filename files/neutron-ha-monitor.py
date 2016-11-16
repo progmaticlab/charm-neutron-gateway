52994e37824803056173ffa5f22cde1fcf89384e
gecong1973 <ge.cong@zte.com.cn>
2016-11-16 15:57:49 +0800

    Replace LOG.warn with LOG.warning
    logging.warn is deprecated in Python 3.[1]
    [1] https://docs.python.org/3/library/logging.html#logging.warning
    
    Change-Id: I9812372ca32497e7806a4b365aef2670ef7ad9af

--- files/neutron-ha-monitor.py
+++ files/neutron-ha-monitor.py
@@ -209,8 +209,8 @@ class MonitorNeutronAgentsDaemon(Daemon):
             return False
 
         if not self.is_same_host(crm_no_1_node):
-            LOG.warn('Only the first crm node %s could reschedule. '
-                     % crm_no_1_node)
+            LOG.warning('Only the first crm node %s could reschedule. '
+                        % crm_no_1_node)
             return False
         return True
 
